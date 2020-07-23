9610777cf2aa901255e32717198cd4cf953e8db3
ssandyy <ssandyy789@gmail.com>
2020-07-23 15:31:25 +0530

    initial

---
+++ backend/models/Departments.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "departments".
+ *
+ * @property int $department_id
+ * @property int $branches_branch_id
+ * @property string $department_name
+ * @property int $companies_company_id
+ * @property string $department_created_date
+ * @property string $department_status
+ *
+ * @property Companies $companiesCompany
+ * @property Branches $branchesBranch
+ */
+class Departments extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'departments';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['branches_branch_id', 'department_name', 'companies_company_id', 'department_status'], 'required'],
+            [['branches_branch_id', 'companies_company_id'], 'integer'],
+            [['department_created_date'], 'safe'],
+            [['department_status'], 'string'],
+            [['department_name'], 'string', 'max' => 255],
+            [['companies_company_id'], 'exist', 'skipOnError' => true, 'targetClass' => Companies::className(), 'targetAttribute' => ['companies_company_id' => 'company_id']],
+            [['branches_branch_id'], 'exist', 'skipOnError' => true, 'targetClass' => Branches::className(), 'targetAttribute' => ['branches_branch_id' => 'branch_id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'department_id' => 'Department ID',
+            'branches_branch_id' => 'Branches Branch ID',
+            'department_name' => 'Department Name',
+            'companies_company_id' => 'Companies Company ID',
+            'department_created_date' => 'Department Created Date',
+            'department_status' => 'Department Status',
+        ];
+    }
+
+    /**
+     * Gets query for [[CompaniesCompany]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCompaniesCompany()
+    {
+        return $this->hasOne(Companies::className(), ['company_id' => 'companies_company_id']);
+    }
+
+    /**
+     * Gets query for [[BranchesBranch]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getBranchesBranch()
+    {
+        return $this->hasOne(Branches::className(), ['branch_id' => 'branches_branch_id']);
+    }
+}
---
+++ frontend/models/SignupForm.php
@@ -0,0 +1,91 @@
+<?php
+namespace frontend\models;
+
+use Yii;
+use yii\base\Model;
+use common\models\User;
+
+/**
+ * Signup form
+ */
+class SignupForm extends Model
+{
+    public $first_name;
+    public $last_name;
+    public $username;
+    public $email;
+    public $password;
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            ['first_name', 'trim'],
+            ['first_name', 'required', 'message'=>'please fill this..'],
+            ['first_name', 'string', 'min' => 2, 'max' => 200],
+
+            ['last_name', 'trim'],
+            ['last_name', 'required'],
+            ['last_name', 'string', 'min' => 2, 'max' => 200],
+
+            ['username', 'trim'],
+            ['username', 'required'],
+            ['username', 'unique', 'targetClass' => '\common\models\User', 'message' => 'This username has already been taken.'],
+            ['username', 'string', 'min' => 2, 'max' => 255],
+
+            ['email', 'trim'],
+            ['email', 'required'],
+            ['email', 'email'],
+            ['email', 'string', 'max' => 255],
+            ['email', 'unique', 'targetClass' => '\common\models\User', 'message' => 'This email address has already been taken.'],
+
+            ['password', 'required'],
+            ['password', 'string', 'min' => 12],
+        ];
+    }
+
+    /**
+     * Signs user up.
+     *
+     * @return bool whether the creating new account was successful and email was sent
+     */
+    public function signup()
+    {
+        if (!$this->validate()) {
+            return null;
+        }
+        
+        $user = new User();
+        $user->first_name = $this->first_name;
+        $user->last_name = $this->last_name;
+        $user->username = $this->username;
+        $user->email = $this->email;
+        $user->setPassword($this->password);
+        $user->generateAuthKey();
+        $user->generateEmailVerificationToken();
+        return $user->save() && $this->sendEmail($user);
+
+    }
+
+    /**
+     * Sends confirmation email to user
+     * @param User $user user model to with email should be send
+     * @return bool whether the email was sent
+     */
+    protected function sendEmail($user)
+    {
+        return Yii::$app
+            ->mailer
+            ->compose(
+                ['html' => 'emailVerify-html', 'text' => 'emailVerify-text'],
+                ['user' => $user]
+            )
+            ->setFrom([Yii::$app->params['supportEmail'] => Yii::$app->name . ' robot'])
+            ->setTo($this->email)
+            ->setSubject('Account registration at ' . Yii::$app->name)
+            ->send();
+    }
+}
